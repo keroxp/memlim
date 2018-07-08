2de0c7fb188492bbe271d7807c1ff0bbdbc594f5
keroxp <kerokerokerop@gmail.com>
2018-07-08 10:00:04 +0900

    test: add teset and fixed codes.

--- dist/index.js
+++ dist/index.js
@@ -1,5 +1,11 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
+var kOverwriteCompareFuncs = {
+    eldestAccess: function (a, b) { return b.lastAccessedAt.getTime() - a.lastAccessedAt.getTime(); },
+    eldest: function (a, b) { return b.createdAt.getTime() - a.createdAt.getTime(); },
+    minSize: function (a, b) { return b.size - a.size; },
+    maxSize: function (a, b) { return a.size - b.size; }
+};
 var Memlim = /** @class */ (function () {
     function Memlim(size, opts) {
         if (opts === void 0) { opts = {
@@ -9,10 +15,32 @@ var Memlim = /** @class */ (function () {
         this.opts = opts;
         this.data = {};
         this.timers = {};
-        this.freeSize = size;
+        this._freeSize = size;
     }
+    Object.defineProperty(Memlim.prototype, "freeSize", {
+        get: function () {
+            return this._freeSize;
+        },
+        enumerable: true,
+        configurable: true
+    });
+    Object.defineProperty(Memlim.prototype, "usedSize", {
+        get: function () {
+            return this.size - this.freeSize;
+        },
+        enumerable: true,
+        configurable: true
+    });
+    Object.defineProperty(Memlim.prototype, "dataCount", {
+        get: function () {
+            return Object.values(this.data).length;
+        },
+        enumerable: true,
+        configurable: true
+    });
     Memlim.prototype.put = function (key, data, ttlMsec) {
         var _this = this;
+        if (ttlMsec === void 0) { ttlMsec = -1; }
         var size = 0;
         if (typeof data === "string") {
             size = data.length * 2;
@@ -23,18 +51,25 @@ var Memlim = /** @class */ (function () {
         else {
             throw new Error("data is not string or ArrayBuffer: " + data);
         }
-        if (this.size < this.freeSize + size) {
+        if (this.freeSize < size) {
             this.ensureSize(size);
         }
         var now = new Date;
+        var createdAt = now;
+        var prevSize = 0;
+        var prevData = this.data[key];
+        if (prevData !== undefined) {
+            prevSize = prevData.size;
+            createdAt = prevData.createdAt;
+        }
         this.data[key] = {
             key: key,
             data: data,
             size: size,
             lastAccessedAt: now,
-            createdAt: now
+            createdAt: createdAt
         };
-        this.freeSize -= size;
+        this._freeSize -= size - prevSize;
         this.clearTimer(key);
         if (ttlMsec > 0) {
             this.timers[key] = setTimeout(function () {
@@ -51,9 +86,9 @@ var Memlim = /** @class */ (function () {
     Memlim.prototype.delete = function (key) {
         if (this.data[key] === undefined)
             return;
-        var _a = this.data[key], data = _a.data, size = _a.size;
+        var size = this.data[key].size;
         delete this.data[key];
-        this.freeSize += size;
+        this._freeSize += size;
         this.clearTimer(key);
     };
     Memlim.prototype.clearTimer = function (key) {
@@ -64,37 +99,26 @@ var Memlim = /** @class */ (function () {
     };
     Memlim.prototype.clear = function () {
         Object.values(this.timers).forEach(clearTimeout);
-        this.freeSize = this.size;
+        this._freeSize = this.size;
         this.timers = {};
         this.data = {};
     };
     Memlim.prototype.ensureSize = function (size) {
-        var list = [];
-        if (this.opts.overwrite === "latestAccess") {
-            list = Object.values(this.data).sort(function (a, b) { return a.lastAccessedAt.getTime() - b.lastAccessedAt.getTime(); });
+        if (this.size < size) {
+            throw new Error("size to be ensured exceeds cache size: " + size + " > " + this.size);
         }
-        else if (this.opts.overwrite === "eldestAccess") {
-            list = Object.values(this.data).sort(function (a, b) { return b.lastAccessedAt.getTime() - a.lastAccessedAt.getTime(); });
+        var compareFunc;
+        if (typeof this.opts.overwrite === "string") {
+            compareFunc = kOverwriteCompareFuncs[this.opts.overwrite];
         }
-        else if (this.opts.overwrite === "latest") {
-            list = Object.values(this.data).sort(function (a, b) { return a.createdAt.getTime() - b.createdAt.getTime(); });
+        else if (typeof this.opts.overwrite === "function") {
+            compareFunc = this.opts.overwrite;
         }
-        else if (this.opts.overwrite === "eldest") {
-            list = Object.values(this.data).sort(function (a, b) { return b.createdAt.getTime() - a.createdAt.getTime(); });
+        if (!compareFunc) {
+            throw new Error("there are no extra space for data: size=" + size);
         }
-        else if (this.opts.overwrite === "minSize") {
-            list = Object.values(this.data).sort(function (a, b) { return a.size - b.size; });
-        }
-        else if (this.opts.overwrite === "maxSize") {
-            list = Object.values(this.data).sort(function (a, b) { return b.size - a.size; });
-        }
-        else if (this.opts.overwrite === "clear") {
-            this.clear();
-        }
-        else {
-            throw new Error("size over.");
-        }
-        while (list.length > 0 && this.size < this.freeSize + size) {
+        var list = Object.values(this.data).sort(compareFunc);
+        while (list.length > 0 && this.freeSize < size) {
             var key = list.pop().key;
             this.delete(key);
         }
--- src/index.ts
+++ src/index.ts
@@ -1,16 +1,40 @@
 type MemlimCacheType = string | ArrayBuffer;
+export type MemlimOverwriteType = "eldestAccess" | "eldest" | "minSize" | "maxSize";
+type CompareFunction<T> = (a: T, b: T) => number;
+const kOverwriteCompareFuncs: {[key: string]: CompareFunction<MemlimEntry<any>>} = {
+    eldestAccess: (a, b) => b.lastAccessedAt.getTime() - a.lastAccessedAt.getTime(),
+    eldest: (a, b) => b.createdAt.getTime() - a.createdAt.getTime(),
+    minSize: (a, b) => b.size - a.size,
+    maxSize: (a, b) => a.size - b.size
+};
+export type MemlimEntry<T extends MemlimCacheType> = {
+    key: string,
+    data: T,
+    size: number,
+    lastAccessedAt: Date,
+    createdAt: Date
+}
 export class Memlim<T extends MemlimCacheType> {
-    data: { [key: string]: { key: string, data: T, size: number, lastAccessedAt: Date, createdAt: Date } } = {}
-    freeSize: number;
-    timers = {};
+    private data: { [key: string]: MemlimEntry<T>} = {};
+    private timers = {};
+    private _freeSize: number;
+    get freeSize() {
+        return this._freeSize;
+    }
+    get usedSize() {
+        return this.size - this.freeSize;
+    }
+    get dataCount() {
+        return Object.values(this.data).length;
+    }
     constructor(
         readonly size: number,
-        readonly opts: { overwrite: "latestAccess" | "eldestAccess" | "latest" | "eldest" | "minSize" | "maxSize" | "clear" } = {
+        readonly opts: { overwrite: MemlimOverwriteType | CompareFunction<MemlimEntry<T>> } = {
             overwrite: "eldestAccess"
         }) {
-        this.freeSize = size;
+        this._freeSize = size;
     }
-    put(key: string, data: T, ttlMsec?: number) {
+    put(key: string, data: T, ttlMsec: number = -1) {
         let size = 0;
         if (typeof data === "string") {
             size = data.length * 2;
@@ -19,24 +43,31 @@ export class Memlim<T extends MemlimCacheType> {
         } else {
             throw new Error(`data is not string or ArrayBuffer: ${data}`);
         }
-        if (this.size < this.freeSize + size) {
+        if (this.freeSize < size) {
             this.ensureSize(size);
         }
         const now = new Date;
+        let createdAt = now;
+        let prevSize = 0;
+        const prevData = this.data[key];
+        if (prevData !== undefined) {
+            prevSize = prevData.size;
+            createdAt = prevData.createdAt;
+        }
         this.data[key] = {
             key,
             data,
             size,
             lastAccessedAt: now,
-            createdAt: now
+            createdAt
         };
-        this.freeSize -= size;
+        this._freeSize -= size - prevSize;
         this.clearTimer(key);
         if (ttlMsec > 0) {
             this.timers[key] = setTimeout(() => {
                 this.delete(key);
             }, ttlMsec)
-        }        
+        }
     }
     get(key: string): T {
         if (this.data[key] === undefined) return;
@@ -45,43 +76,38 @@ export class Memlim<T extends MemlimCacheType> {
     }
     delete(key: string) {
         if (this.data[key] === undefined) return;
-        const { data, size } = this.data[key];
-        delete this.data[key]
-        this.freeSize += size;
+        const { size } = this.data[key];
+        delete this.data[key];
+        this._freeSize += size;
         this.clearTimer(key);
     }
     private clearTimer(key) {
         if (this.timers[key]) {
-            clearTimeout(this.timers[key])
+            clearTimeout(this.timers[key]);
             delete this.timers[key];
         }
     }
     clear() {
-        Object.values(this.timers).forEach(clearTimeout);        
-        this.freeSize = this.size;
+        Object.values(this.timers).forEach(clearTimeout);
+        this._freeSize = this.size;
         this.timers = {};
         this.data = {}
     }
     private ensureSize(size) {
-        let list = [];
-        if (this.opts.overwrite === "latestAccess") {
-            list = Object.values(this.data).sort((a, b) => a.lastAccessedAt.getTime() - b.lastAccessedAt.getTime());
-        } else if (this.opts.overwrite === "eldestAccess") {
-            list = Object.values(this.data).sort((a, b) => b.lastAccessedAt.getTime() - a.lastAccessedAt.getTime());
-        } else if (this.opts.overwrite === "latest") {
-            list = Object.values(this.data).sort((a, b) => a.createdAt.getTime() - b.createdAt.getTime());
-        } else if (this.opts.overwrite === "eldest") {
-            list = Object.values(this.data).sort((a, b) => b.createdAt.getTime() - a.createdAt.getTime());
-        } else if (this.opts.overwrite === "minSize") {
-            list = Object.values(this.data).sort((a, b) => a.size - b.size);
-        } else if (this.opts.overwrite === "maxSize") {
-            list = Object.values(this.data).sort((a, b) => b.size - a.size);
-        } else if (this.opts.overwrite === "clear") { 
-            this.clear();
-        } else {
-            throw new Error("size over.");
+        if (this.size < size) {
+            throw new Error(`size to be ensured exceeds cache size: ${size} > ${this.size}`);
         }
-        while (list.length > 0 && this.size < this.freeSize + size) {
+        let compareFunc;
+        if (typeof this.opts.overwrite === "string") {
+            compareFunc = kOverwriteCompareFuncs[this.opts.overwrite];
+        } else if (typeof this.opts.overwrite === "function") {
+            compareFunc = this.opts.overwrite;
+        }
+        if (!compareFunc) {
+            throw new Error(`there are no extra space for data: size=${size}`);
+        }
+        const list = Object.values(this.data).sort(compareFunc);
+        while (list.length > 0 && this.freeSize < size) {
             const { key } = list.pop();
             this.delete(key);
         }
